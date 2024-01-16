6dcda859334399204f722bf822797e1b172409ff
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-01-16 10:50:24 +0100

    Add module with test

---
+++ source/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="common-python-packages",
+    version="0.0.1",
+    description="Common Python packages",
+    long_description="",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "ConfigArgParse==1.5.3",
+    ],
+)
