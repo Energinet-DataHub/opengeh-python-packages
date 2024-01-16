2b720d7e3cf5fa3976d54e9a41d97df29e1be384
Søren Søndergaard <s.sondergaard@icloud.com>
2024-01-16 17:23:57 +0100

    Initial setup for CI (#3)
    * Initial setup for CI
    
    * Setup python folders
    
    * Configure CD pipeline
    
    * Configure setup.py
    
    * Add add file

--- source/spark-sql-migrations/setup.py
+++ source/spark-sql-migrations/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="common-python-packages",
+    name="opengeh-spark-sql-migrations",
     version="0.1.1",
-    description="Common Python packages",
+    description="opengeh spark sql migrations package",
     long_description="",
     long_description_content_type="text/markdown",
     license="MIT",
---
+++ source/telemetry/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="opengeh-telemetry",
+    version="0.1.1",
+    description="opengeh telemetry package",
+    long_description="",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+)
