0b9e6eaba12592a53c6601c2d9fd6b8f16f6d125
Søren Søndergaard <s.sondergaard@icloud.com>
2024-01-16 15:40:33 +0100

    Configure setup.py

---
+++ source/spark-sql-migrations/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="opengeh-spark-sql-migrations",
+    version="0.1.1",
+    description="opengeh spark sql migrations package",
+    long_description="",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+)
