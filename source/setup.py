accdbd81a1b2ad46ae23b88564231d11d6425640
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-01-16 12:10:57 +0100

    bump

--- source/setup.py
+++ source/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="common-python-packages",
-    version="0.1",
+    version="0.1.1",
     description="Common Python packages",
     long_description="",
     long_description_content_type="text/markdown",
