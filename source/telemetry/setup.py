8f4c633ec4f095467374028c9920146ca5c43eab
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-01-22 11:05:44 +0100

    remove unused packages

--- source/telemetry/setup.py
+++ source/telemetry/setup.py
@@ -10,14 +10,5 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "ConfigArgParse==1.5.3",
-        "pyspark==3.3.0",
-        "azure-identity==1.11.0",
-        "azure-storage-file-datalake==12.9.1",
-        "azure-storage-blob==12.14.1",
-        "databricks-cli==0.17.3",
-        "delta-spark==2.3.0",
-        "dependency_injector==4.41.0",
-        "azure-monitor-opentelemetry==1.0.0",
     ],
 )
