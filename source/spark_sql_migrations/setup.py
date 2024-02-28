5576ecab64bd33ed65bd766d53536c3f10d667f6
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-02-28 10:48:57 +0100

    clean up

--- source/spark_sql_migrations/setup.py
+++ source/spark_sql_migrations/setup.py
@@ -10,7 +10,6 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "ConfigArgParse==1.5.3",
         "pyspark==3.5.1",
         "delta-spark==3.1.0",
         "dependency_injector==4.41.0",
