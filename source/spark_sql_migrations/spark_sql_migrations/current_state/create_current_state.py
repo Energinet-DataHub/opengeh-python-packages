31a43637be6b85519f61e7c6d65ea7553c479d1a
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-03-14 13:13:35 +0100

    Bump Pyspark to 3.5.0 (#18)
    Bump Pyspark to 3.5.0

--- source/spark_sql_migrations/spark_sql_migrations/current_state/create_current_state.py
+++ source/spark_sql_migrations/spark_sql_migrations/current_state/create_current_state.py
@@ -11,12 +11,20 @@ def create_all_tables() -> None:
 
 @inject
 def _create_all_tables(
-    config: Configuration = Provide[SparkSqlMigrationsContainer.config]
+    config: Configuration = Provide[SparkSqlMigrationsContainer.config],
 ) -> None:
     """Executes schema and table scripts to create all tables"""
     print("Creating all tables")
+
     schema_scripts = _get_schema_scripts()
     table_scripts = _get_table_scripts()
+
+    print(
+        f"Found {len(schema_scripts)} schema scripts in {config.current_state_schemas_folder_path}"
+    )
+    print(
+        f"Found {len(table_scripts)} table scripts in {config.current_state_tables_folder_path}"
+    )
 
     for script in schema_scripts:
         sql_file_executor.execute(script, config.current_state_schemas_folder_path)
@@ -29,15 +37,19 @@ def _create_all_tables(
 
 @inject
 def _get_schema_scripts(
-        config: Configuration = Provide[SparkSqlMigrationsContainer.config]
+    config: Configuration = Provide[SparkSqlMigrationsContainer.config],
 ) -> list[str]:
     migration_files = contents(config.current_state_schemas_folder_path)
-    return [file.removesuffix(".sql") for file in migration_files if file.endswith(".sql")]
+    return [
+        file.removesuffix(".sql") for file in migration_files if file.endswith(".sql")
+    ]
 
 
 @inject
 def _get_table_scripts(
-    config: Configuration = Provide[SparkSqlMigrationsContainer.config]
+    config: Configuration = Provide[SparkSqlMigrationsContainer.config],
 ) -> list[str]:
     migration_files = contents(config.current_state_tables_folder_path)
-    return [file.removesuffix(".sql") for file in migration_files if file.endswith(".sql")]
+    return [
+        file.removesuffix(".sql") for file in migration_files if file.endswith(".sql")
+    ]
