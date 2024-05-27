cfbec5be7cbf7c12d146d4e62bc54fa762d6e6ae
Jan Duelund <jdd@energinet.dk>
2024-05-27 12:03:31 +0200

    builder

--- source/spark_sql_migrations/tests/builders/configuration_builder.py
+++ source/spark_sql_migrations/tests/builders/configuration_builder.py
@@ -11,6 +11,7 @@ substitution_variables = {"substitution_variables": "substitution_variables"}
 def build(
     current_state_schemas_folder_path: str = "tests.test_scripts.schema_scripts",
     current_state_tables_folder_path: str = "tests.test_scripts.table_scripts",
+    current_state_views_folder_path: str = "tests.test_scripts.views_scripts",
     migration_scripts_folder_path: str = "tests.test_scripts.migration_scripts",
     migration_schema_name: str = SchemaMigrationConstants.schema_name,
     migration_table_name: str = SchemaMigrationConstants.table_name,
@@ -30,6 +31,7 @@ def build(
     spark_config = spark_sql_migrations_configuration_builder.build(
         current_state_schemas_folder_path=current_state_schemas_folder_path,
         current_state_tables_folder_path=current_state_tables_folder_path,
+        current_state_views_folder_path=current_state_views_folder_path,
         migration_scripts_folder_path=migration_scripts_folder_path,
         migration_schema_name=migration_schema_name,
         migration_table_name=migration_table_name,
--- source/spark_sql_migrations/tests/helpers/schema_migration_costants.py
+++ source/spark_sql_migrations/tests/helpers/schema_migration_costants.py
@@ -6,3 +6,4 @@ class SchemaMigrationConstants:
     migration_scripts_folder_path = "package.schema_migration.migration_scripts"
     current_state_schemas_folder_path = "package.schema_migration.current_state_scripts.schemas"
     current_state_tables_folder_path = "package.schema_migration.current_state_scripts.tables"
+    current_state_views_folder_path = "package.schema_migration.current_state_scripts.views"
