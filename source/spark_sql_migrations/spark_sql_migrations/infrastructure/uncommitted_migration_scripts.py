be3a5c559d914ce1ec6be670bd2b205288a495c7
Jan Duelund <jdd@energinet.dk>
2024-05-28 14:49:32 +0200

    Update uncommitted_migration_scripts.py

--- source/spark_sql_migrations/spark_sql_migrations/infrastructure/uncommitted_migration_scripts.py
+++ source/spark_sql_migrations/spark_sql_migrations/infrastructure/uncommitted_migration_scripts.py
@@ -52,6 +52,16 @@ def _create_schema_migration_table(
     spark: SparkSession = Provide[SparkSqlMigrationsContainer.spark],
     config: Configuration = Provide[SparkSqlMigrationsContainer.config],
 ) -> None:
+    if config.unity_catalog_enabled:
+        delta_table_helper.create_table_from_schema(
+            spark,
+            config.catalog_name,
+            schema_name,
+            table_name,
+            schema_migration_schema,
+        )
+        return
+
     delta_table_helper.create_schema(
         spark,
         config.catalog_name,
