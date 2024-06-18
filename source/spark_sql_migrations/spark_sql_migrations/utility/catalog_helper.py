485415d51b9aed32addd352cb39961ca46b05957
Jan Duelund <jdd@energinet.dk>
2024-06-18 09:01:20 +0200

    Update catalog_helper.py

--- source/spark_sql_migrations/spark_sql_migrations/utility/catalog_helper.py
+++ source/spark_sql_migrations/spark_sql_migrations/utility/catalog_helper.py
@@ -6,9 +6,11 @@ def is_unity_catalog(
         catalog_name: str,
 ) -> bool:
     if not spark.catalog.databaseExists("system.information_schema"):
-        # If the system catalog does not exist, then the workspace is not a Unity enabled workspace
+        # If the system.information schema does not exist, then the workspace is not a Unity enabled workspace
         return False
-
     result = spark.sql(f'SELECT count(*) AS count FROM system.information_schema.catalogs'
-                       f' WHERE catalog_name = "{catalog_name}"').collect()[0][0]
-    return result > 0
+                       f' WHERE catalog_name = "{catalog_name}"')
+    first_row = 0
+    count_col = 0
+    count = result.collect()[first_row][count_col]
+    return count > 0
