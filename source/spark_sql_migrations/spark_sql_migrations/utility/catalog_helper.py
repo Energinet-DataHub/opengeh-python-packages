bd4c843c393edbb207cae3ce954ed516fe946fe1
Jan Duelund <jdd@energinet.dk>
2024-06-18 09:07:43 +0200

    fix: minor bugfix in unity catalog detection (#32)
    * minor bugfix
    
    * fix test
    
    * Update catalog_helper.py
    
    * Update uncommitted_migration_scripts.py
    
    * Update catalog_helper.py

--- source/spark_sql_migrations/spark_sql_migrations/utility/catalog_helper.py
+++ source/spark_sql_migrations/spark_sql_migrations/utility/catalog_helper.py
@@ -5,12 +5,12 @@ def is_unity_catalog(
         spark: SparkSession,
         catalog_name: str,
 ) -> bool:
-    if not spark.catalog.databaseExists("system"):
-        # If the system catalog does not exist, then the workspace is not a Unity enabled workspace
+    if not spark.catalog.databaseExists("system.information_schema"):
+        # If the system.information schema does not exist, then the workspace is not a Unity enabled workspace
         return False
-
-    sql_command = (f'SELECT count(*) AS count FROM system.information_schema.catalogs'
-                   f' WHERE catalog_name = {catalog_name}')
-    result = spark.sql(sql_command)
-    count = result.collect()[0][0]
+    result = spark.sql(f'SELECT count(*) AS count FROM system.information_schema.catalogs'
+                       f' WHERE catalog_name = "{catalog_name}"')
+    first_row = 0
+    count_col = 0
+    count = result.collect()[first_row][count_col]
     return count > 0
