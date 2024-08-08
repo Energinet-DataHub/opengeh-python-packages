ba8ea12d2e3c39a165870ade260892a545b02674
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-08-08 08:21:40 +0200

    add schema to views

--- source/spark_sql_migrations/spark_sql_migrations/models/view.py
+++ source/spark_sql_migrations/spark_sql_migrations/models/view.py
@@ -1,3 +1,7 @@
+from pyspark.sql.types import StructType
+
+
 class View:
-    def __init__(self, name: str):
+    def __init__(self, name: str, schema: StructType):
         self.name = name
+        self.schema = schema
