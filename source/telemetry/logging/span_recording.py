4539f78e0c5d594d97d2efd72722b93fb96a7c84
Christian Hansen <cenh@halfspace.io>
2024-11-04 14:34:49 +0100

    updated

--- source/telemetry/logging/span_recording.py
+++ source/telemetry/logging/span_recording.py
@@ -1,5 +1,5 @@
 from opentelemetry.trace import Status, StatusCode, Span
-import logging.logging_configuration as config
+import telemetry.logging.logging_configuration as config
 
 
 def span_record_exception(exception: SystemExit | Exception, span: Span) -> None:
--- source/telemetry/setup.py
+++ source/telemetry/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opengeh-telemetry",
-    version="0.1.2",
+    version="2.1.0",
     description="opengeh telemetry package",
     long_description="",
     long_description_content_type="text/markdown",
