c0451cd90cb48bf34b7a11d3646850f7766166c0
Christian Hansen <cenh@halfspace.io>
2024-11-21 10:57:14 +0100

    added

---
+++ source/telemetry/tests/conftest.py
@@ -0,0 +1,88 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+from pathlib import Path
+import logging
+import pytest
+import yaml
+from integration_test_configuration import IntegrationTestConfiguration
+
+@pytest.fixture(scope="session")
+def telemetry_path() -> str:
+    """
+    Returns the source/telemetry/ folder path.
+    Please note that this only works if current folder haven't been changed prior using
+    `os.chdir()`. The correctness also relies on the prerequisite that this function is
+    actually located in a file located directly in the tests folder.
+    """
+    return "source/telemetry"
+
+@pytest.fixture(scope="session")
+def telemetry_tests_path(telemetry_path: str) -> str:
+    """
+    Returns the tests folder path.
+    Please note that this only works if current folder haven't been changed prior using
+    `os.chdir()`. The correctness also relies on the prerequisite that this function is
+    actually located in a file located directly in the tests folder.
+    """
+    return f"{telemetry_path}/tests"
+
+@pytest.fixture(scope="session")
+def integration_test_configuration(telemetry_tests_path: str) -> IntegrationTestConfiguration:
+    """
+    Load settings for integration tests either from a local YAML settings file or from environment variables.
+    Proceeds even if certain Azure-related keys are not present in the settings file.
+    """
+
+    settings_file_path = Path(telemetry_tests_path) / "integrationtest.local.settings.yml"
+
+    def load_settings_from_env() -> dict:
+        return {
+            key: os.getenv(key)
+            for key in [
+                "AZURE_KEYVAULT_URL",
+                "AZURE_CLIENT_ID",
+                "AZURE_CLIENT_SECRET",
+                "AZURE_TENANT_ID",
+                "AZURE_SUBSCRIPTION_ID",
+            ]
+            if os.getenv(key) is not None
+        }
+
+    settings = _load_settings_from_file(settings_file_path) or load_settings_from_env()
+
+    # Set environment variables from loaded settings
+    for key, value in settings.items():
+        if value is not None:
+            os.environ[key] = value
+
+    if "AZURE_KEYVAULT_URL" in settings:
+        return IntegrationTestConfiguration(
+            azure_keyvault_url=settings["AZURE_KEYVAULT_URL"]
+        )
+
+    logging.error(
+        f"Integration test configuration could not be loaded from {settings_file_path} or environment variables."
+    )
+    raise Exception(
+        "Failed to load integration test settings. Ensure that the Azure Key Vault URL is provided in the settings file or as an environment variable."
+    )
+
+
+def _load_settings_from_file(file_path: Path) -> dict:
+    if file_path.exists():
+        with file_path.open() as stream:
+            return yaml.safe_load(stream)
+    else:
+        return {}
