6afac6b0a69056b58ad9e0d3490fcec02697e544
Jonas Michelsen <jvm@energinet.dk>
2024-11-21 12:12:16 +0100

    refac: bump telemetry

--- source/telemetry/release-notes/release-notes.md
+++ source/telemetry/release-notes/release-notes.md
@@ -1,5 +1,10 @@
 # Telemetry Release Notes
 
+## Version 2.1.1
+
+- Bump azure-monitor-opentelemetry required version from 1.6.0 to 1.6.4
+- Bump azure-core required version from 1.30.0 to 1.32.0
+
 ## Version 2.1.0
 
 - Added shared telemtry library for logging
--- source/telemetry/setup.py
+++ source/telemetry/setup.py
@@ -10,7 +10,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "azure-monitor-opentelemetry==1.6.0",
-        "azure-core==1.30.0",
+        "azure-monitor-opentelemetry==1.6.4",
+        "azure-core==1.32.0",
     ],
 )
