9789ad0f130b69faf78a79a4bdd2f1d758cd9dcb
Jens Peder Meldgaard <jpm@halfspace.io>
2024-12-18 16:17:53 +0100

    feat: add assert_schemas
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/testcommon/testcommon/__init__.py
+++ source/testcommon/testcommon/__init__.py
@@ -6,6 +6,13 @@ from testcommon.get_then_names import get_then_names
 # This function has A LOT of dependency issues. These have been resolved in
 # the setup.py file. So we can import the function here without any issues.
 from pyspark.testing.utils import assertDataFrameEqual as assert_dataframes
+from pyspark.testing.utils import assertSchemaEqual as assert_schemas
 
 
-__all__ = ["TestCases", "read_csv", "assert_dataframes", "get_then_names"]
+__all__ = [
+    "TestCases",
+    "read_csv",
+    "assert_dataframes",
+    "get_then_names",
+    "assert_schemas",
+]
