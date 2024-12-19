57c2398075eb72b5f3a2a0bf505700cbff43827b
Jens Peder Meldgaard <jpm@halfspace.io>
2024-12-19 09:24:03 +0100

    chore: separate coverage and etl utilities
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

---
+++ source/testcommon/testcommon/covernator/__init__.py
@@ -0,0 +1,5 @@
+from testcommon.covernator.TestCases import TestCases
+
+__all__ = [
+    "TestCases",
+]
---
+++ source/testcommon/testcommon/etl/__init__.py
@@ -0,0 +1,15 @@
+from testcommon.etl.read_csv import read_csv
+from testcommon.etl.get_then_names import get_then_names
+
+# We are using the assertDataFrameEqual function from PySpark.
+# This function has A LOT of dependency issues. These have been resolved in
+# the setup.py file. So we can import the function here without any issues.
+from pyspark.testing.utils import assertDataFrameEqual as assert_dataframes
+from pyspark.testing.utils import assertSchemaEqual as assert_schemas
+
+__all__ = [
+    "read_csv",
+    "assert_dataframes",
+    "get_then_names",
+    "assert_schemas",
+]
