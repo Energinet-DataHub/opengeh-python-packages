26266b6f39d2773f2b34ce075acc663c800d8dd6
Bjarke Meier <hej@bjarkemeier.dk>
2025-01-03 14:56:42 +0100

    remove obsolete - heavy - deps

--- source/testcommon/setup.py
+++ source/testcommon/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opengeh-testcommon",
-    version="0.0.2",
+    version="0.0.4",
     description="Shared testing utilities for OpenGEH Python packages",
     long_description="",
     long_description_content_type="text/markdown",
@@ -14,10 +14,5 @@ setup(
         "pytest",
         "PyYAML",
         "delta-spark",
-        # The pandas, numpy and PyArray dependencies are required for using
-        # `assertDataFrameEqual` and `assertSchemaEqual` function from PySpark.
-        "pandas>=1.0.5,<2.0.0",
-        "numpy==1.26.4",
-        "PyArrow>=4.0.0",
     ],
 )
