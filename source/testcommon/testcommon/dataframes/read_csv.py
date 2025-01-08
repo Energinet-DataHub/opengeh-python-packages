f2de2aa00c4da06dc2ebd685cac10802ce4fdfeb
Jens Peder Meldgaard <jpm@halfspace.io>
2025-01-08 15:59:33 +0100

    fix: columns should be ignorable
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/testcommon/testcommon/dataframes/read_csv.py
+++ source/testcommon/testcommon/dataframes/read_csv.py
@@ -3,7 +3,11 @@ from pyspark.sql import types as T, functions as F
 
 
 def read_csv(
-    spark: SparkSession, path: str, schema: T.StructType, sep: str = ";"
+    spark: SparkSession,
+    path: str,
+    schema: T.StructType,
+    sep: str = ";",
+    ignored_value="[IGNORED]",
 ) -> DataFrame:
     """Read a CSV file into a Spark DataFrame.
 
@@ -12,20 +16,31 @@ def read_csv(
         path (str): The path to the CSV file.
         schema (StructType): The schema of the CSV file.
         sep (str, optional): The separator of the CSV file. Defaults to ";".
+        ignored_value (str, optional): Columns where all rows is equal to the
+            ignored_value will be removed from the resulting DataFrame.
+            Defaults to "[IGNORED]".
 
     Returns:
         DataFrame: The Spark DataFrame.
     """
     raw_df = spark.read.csv(path, header=True, sep=sep)
 
+    ignore_check = raw_df.agg(
+        *[F.every(F.col(c) == F.lit(ignored_value)).alias(c) for c in raw_df.columns]
+    ).collect()
+
+    ignored_cols = [
+        c for c, v in ignore_check[0].asDict().items() if v and c in schema.fieldNames()
+    ]
+
+    raw_df = raw_df.drop(*ignored_cols)
+
     transforms = []
     for field in schema.fields:
         if field.name in raw_df.columns:
             if isinstance(field.dataType, T.ArrayType):
                 transforms.append(
-                    F.from_json(F.col(field.name), field.dataType).alias(
-                        field.name
-                    )
+                    F.from_json(F.col(field.name), field.dataType).alias(field.name)
                 )
             else:
                 transforms.append(
@@ -38,9 +53,7 @@ def read_csv(
 
 def _fix_nullable(df: DataFrame, schema: T.StructType):
     for field in df.schema.fields:
-        assert (
-            field.name in schema.fieldNames()
-        ), f"Field {field.name} not in schema"
+        assert field.name in schema.fieldNames(), f"Field {field.name} not in schema"
         if isinstance(schema[field.name].dataType, T.ArrayType):
             df.schema[field.name].dataType = schema[field.name].dataType
         else:
