a53dc0887cde5683f6fd99414e87a5034f2d050d
chba <chba@kapacity.dk>
2025-01-16 14:57:36 +0100

    code now works after being joined with Thomas'

--- source/decorator_hello_world.py
+++ source/decorator_hello_world.py
@@ -13,33 +13,31 @@ def run_method():
     log.info(f"Inside method")
     log.warning("I am now a warning inside the app")
 
-@logging_decorators.use_logging
-def entry_point(span=None) -> None:
+@logging_decorators.start_trace
+def entry_point(initial_span=None) -> None:
     try:
         run_method()
     except Exception as e:
         log = Logger(__name__)
         log.error(str(e))
 
-
+# Set up environment variables for testing
 os.environ['CLOUD_ROLE_NAME'] = 'dbr-electrical-heating'
 os.environ['APPLICATIONINSIGHTS_CONNECTION_STRING'] = 'test_string'
 os.environ['SUBSYSTEM'] = 'electrical-heating'
 
-log_settings = logging_configuration.LoggingSettings.load()
-#print(log_settings)
+if __name__ == '__main__':
+    # Create settings for logging using the LoggingSettings class
+    log_settings = logging_configuration.LoggingSettings.load()
 
-# logging_configuration.configure_logging(
-#     cloud_role_name=log_settings.cloud_role_name,
-#     tracer_name=log_settings.tracer_name,
-#     applicationinsights_connection_string=log_settings.applicationinsights_connection_string,
-#     extras=log_settings.logging_extras,
-#     force_configuration=log_settings.force_configuration
-# )
-# entry_point()
-
-
-
-
-
-
+    # TODO: Force usage of LoggingSettings as input object for configure_logging()
+    # Call configure_logging
+    logging_configuration.configure_logging(
+        cloud_role_name=log_settings.cloud_role_name,
+        tracer_name=log_settings.subsystem,
+        #applicationinsights_connection_string=log_settings.applicationinsights_connection_string,
+        applicationinsights_connection_string=None,
+        extras=log_settings.logging_extras
+    )
+    # Call Entry point
+    entry_point()
