59374dce8f932541d37027bb5ce125cc2e5012da
Jakob Stricker Nielsen <jakob.s.nielsen@twoday.com>
2025-01-20 14:41:32 +0100

    Update the view_scenario_executor.py to use be able to handle views in the Wholesale repo

--- source/testcommon/testcommon/utils/views/view_scenario_executor.py
+++ source/testcommon/testcommon/utils/views/view_scenario_executor.py
@@ -1,23 +1,10 @@
-# Copyright 2020 Energinet DataHub A/S
-#
-# Licensed under the Apache License, Version 2.0 (the "License2");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from typing import Tuple
 
 from pyspark.sql import SparkSession
 
-from tests.features.utils.csv_to_dataframe_parser import CsvToDataframeWrapperParser
-from tests.features.utils.dataframes.typecasting import cast_column_types
-from tests.features.utils.views.dataframe_wrapper import DataframeWrapper
+from source.testcommon.testcommon.utils.csv_to_dataframe_parser import CsvToDataframeWrapperParser
+from source.testcommon.testcommon.utils.dataframes.typecasting import cast_column_types
+from source.testcommon.testcommon.utils.views.dataframe_wrapper import DataframeWrapper
 
 
 class ViewScenarioExecutor:
@@ -27,27 +14,21 @@ class ViewScenarioExecutor:
         self.spark = spark
         self.parser = CsvToDataframeWrapperParser(spark)
 
-    def execute(
-        self, scenario_folder_path: str
-    ) -> Tuple[list[DataframeWrapper], list[DataframeWrapper]]:
-
+    def execute(self, scenario_folder_path: str) -> list[DataframeWrapper]:
         input_dataframes_wrappers = self.parser.parse_csv_files_concurrently(
             f"{scenario_folder_path}/when"
         )
 
-        input_dataframes_wrappers = self.correct_dataframe_types(
-            input_dataframes_wrappers
-        )
+        self.types = self.correct_dataframe_types(input_dataframes_wrappers)
+        input_dataframes_wrappers = self.types
         self._write_to_tables(input_dataframes_wrappers)
 
         output_dataframe_wrappers = self.parser.parse_csv_files_concurrently(
             f"{scenario_folder_path}/then"
         )
 
-        expected = self.correct_dataframe_types(output_dataframe_wrappers)
-
         actual = self._read_from_views(output_dataframe_wrappers)
-        return actual, expected
+        return actual
 
     @staticmethod
     def _write_to_tables(
