64b15556ebe9761d4a597a5c72e6bde1d36d2946
Thomas Erland Clausen <twotcl@inox.dk>
2025-01-20 12:35:49 +0100

    changing dep to build from feature branch

--- source/telemetry/tests/telemetry_logging/test_decorators.py
+++ source/telemetry/tests/telemetry_logging/test_decorators.py
@@ -65,9 +65,8 @@ def test_start_trace__when_logging_not_configured(mock_logger, mock_start_trace)
     mock_start_trace_instance = mock_start_trace.return_value
 
     # Prepare
-    @start_trace()
+    @start_trace
     def app_sample_function(initial_span=None):
-        _LOGGING_CONFIGURED = True
         assert (1 + 1) == 2
         return "I am an app sample function. Doing important calculations"
 
