64306c1cb0de4a6a6baebf20672aa21c67e1a3a7
jesper knudsen <jmk@halfspace.io>
2025-01-21 10:06:42 +0100

    fix

--- source/pyspark_functions/setup.py
+++ source/pyspark_functions/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="opengeh-pyspark-functions",
+    name="opengeh-pyspark",
     version="0.0.1",
     description="Shared pyspark functions for OpenGEH",
     long_description="",
