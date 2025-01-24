69abd640fcff9289e7465f8f61b6b285feddbbc4
Jakob Stricker Nielsen <jakob.s.nielsen@twoday.com>
2025-01-24 11:02:06 +0100

    get_then_names.py

--- source/testcommon/testcommon/etl/get_then_names.py
+++ source/testcommon/testcommon/etl/get_then_names.py
@@ -8,19 +8,23 @@ def get_then_names(scenario_path = None) -> list[str]:
     Returns:
         list[str]: A list of file paths relative to the `then` folder, without file extensions.
     """
-    if scenario_path is None:
-        test_file_path = inspect.stack()[1].filename
-        output_folder_path = Path(test_file_path).parent / "then"
-    else:
+
+    test_file_path = inspect.stack()[1].filename
+    output_folder_path = Path(test_file_path).parent
+    then_output_folder_path = output_folder_path / "then"
+
+    if scenario_path is not None:
         test_file_path = scenario_path
-        output_folder_path = Path(test_file_path) / "then"
+        output_folder_path = Path(test_file_path)
+        then_output_folder_path = output_folder_path / "then"
+
     if not output_folder_path.exists():
         raise FileNotFoundError(
-            f"Could not find the 'then' folder in {Path(test_file_path).parent}"
+            f"Could not find the 'then' folder in {output_folder_path}"
         )
-    then_files = list(output_folder_path.rglob("*.csv"))
+    then_files = list(then_output_folder_path.rglob("*.csv"))
     if not then_files:
         raise FileNotFoundError(
-            f"Could not find any CSV files in the 'then' folder in {Path(test_file_path).parent}"
+            f"Could not find any CSV files in the 'then' folder in {output_folder_path}"
         )
-    return [str(f.relative_to(output_folder_path).with_suffix('')) for f in then_files]
+    return [str(f.relative_to(then_output_folder_path).with_suffix('')) for f in then_files]
