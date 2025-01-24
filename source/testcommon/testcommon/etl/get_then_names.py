24e46e184053b2f57ea9589e15131178c61e082f
Jakob Stricker Nielsen <jakob.s.nielsen@twoday.com>
2025-01-24 10:25:44 +0100

    get_then_names.py

--- source/testcommon/testcommon/etl/get_then_names.py
+++ source/testcommon/testcommon/etl/get_then_names.py
@@ -2,13 +2,16 @@ import inspect
 from pathlib import Path
 
 
-def get_then_names() -> list[str]:
+def get_then_names(scenario_path = None) -> list[str]:
     """Get the names of the CSV files in the `then` folder in an ETL test.
 
     Returns:
         list[str]: A list of file paths relative to the `then` folder, without file extensions.
     """
-    test_file_path = inspect.stack()[1].filename
+    if scenario_path is None:
+        test_file_path = inspect.stack()[1].filename
+    else:
+        test_file_path = scenario_path
     output_folder_path = Path(test_file_path).parent / "then"
     if not output_folder_path.exists():
         raise FileNotFoundError(
