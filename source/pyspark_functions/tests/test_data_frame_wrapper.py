ff6f308bc78c5b4c76812b685d2fc712e5c72f4f
Christian Hansen <cenh@halfspace.io>
2025-01-30 08:58:08 +0100

    updated names

--- source/pyspark_functions/tests/test_data_frame_wrapper.py
+++ source/pyspark_functions/tests/test_data_frame_wrapper.py
@@ -1,14 +1,22 @@
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+    DoubleType,
+)
 from pyspark_functions.data_frame_wrapper import DataFrameWrapper
 import pytest
 
 
-def test__data_frame_wrapper_initialization(spark):
+def test__ctor__when_valid_input__returns_expected_schema_and_data(spark):
     # Arrange
-    schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), True)
-    ])
+    schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+        ]
+    )
     data = [("Alice", 30), ("Bob", 25)]
     df = spark.createDataFrame(data, schema)
 
@@ -20,19 +28,23 @@ def test__data_frame_wrapper_initialization(spark):
     assert wrapper.df.collect() == df.collect()
 
 
-def test__data_frame_wrapper_keeps_columns_from_expected_schema(spark):
+def test__ctor__when_different_columns__returns_expected_schema(spark):
     # Arrange
-    initial_schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), True),
-        StructField("city", StringType(), True),
-    ])
+    initial_schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+            StructField("city", StringType(), True),
+        ]
+    )
     data = [("Alice", 30, "Roskilde"), ("Bob", 25, "Taastrup")]
     df = spark.createDataFrame(data, initial_schema)
-    expected_schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), True),
-    ])
+    expected_schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+        ]
+    )
     expected_data = [("Alice", 30), ("Bob", 25)]
     expected_df = spark.createDataFrame(expected_data, expected_schema)
 
@@ -44,20 +56,24 @@ def test__data_frame_wrapper_keeps_columns_from_expected_schema(spark):
     assert wrapper.df.collect() == expected_df.collect()
 
 
-def test__data_frame_wrapper_missing_nullable_columns(spark):
+def test__ctor__when_nullable_column__returns_column_filled_with_nulls(spark):
     # Arrange
-    schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), True),
-    ])
+    schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+        ]
+    )
     data = [("Alice", 30), ("Bob", None)]
     df = spark.createDataFrame(data, schema=schema)
 
-    expected_schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), True),
-        StructField("city", StringType(), True),
-    ])
+    expected_schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+            StructField("city", StringType(), True),
+        ]
+    )
     expected_data = [("Alice", 30, None), ("Bob", None, None)]
     expected_df = spark.createDataFrame(expected_data, expected_schema)
 
@@ -68,29 +84,39 @@ def test__data_frame_wrapper_missing_nullable_columns(spark):
     assert wrapper.df.collect() == expected_df.collect()
 
 
-def test__data_frame_wrapper_unexpected_nullability(spark):
+def test__ctor__when_not_nullable_column_and_null_value__throws_exception(
+    spark,
+):
     # Arrange
-    schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), True),
-    ])
+    schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+        ]
+    )
     data = [("Alice", 30), ("Bob", None)]
     df = spark.createDataFrame(data, schema=schema)
-    used_schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), False),
-    ])
+    used_schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), False),
+        ]
+    )
 
     # Act & Assert
-    with pytest.raises(AssertionError, match="Expected column name 'age' to have nullable"):
+    with pytest.raises(
+        AssertionError, match="Expected column name 'age' to have nullable"
+    ):
         DataFrameWrapper(df, used_schema)
 
 
-def test__data_frame_wrapper_cache_internal(spark):
-    schema = StructType([
-        StructField("name", StringType(), True),
-        StructField("age", IntegerType(), True)
-    ])
+def test__ctor__when_cache_internal__dataframe_is_cached(spark):
+    schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+        ]
+    )
     data = [("Alice", 30), ("Bob", 25)]
     df = spark.createDataFrame(data, schema)
 
