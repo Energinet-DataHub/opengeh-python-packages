5300be27d296b1ffc57db378d771810d79d97500
chba <chba@kapacity.dk>
2025-02-03 10:53:26 +0100

    making extras default to none

--- source/telemetry/telemetry_logging/logging_configuration.py
+++ source/telemetry/telemetry_logging/logging_configuration.py
@@ -48,7 +48,7 @@ class LoggingSettings(BaseSettings):
     The priority is CLI parameters first and then environment variables.
     """
     cloud_role_name: str
-    applicationinsights_connection_string: str = Field(alias="`APPLICATIONINSIGHTS-CONNECTION-STRING`") | None
+    applicationinsights_connection_string: str = Field(alias="APPLICATIONINSIGHTS-CONNECTION-STRING") | None
     subsystem: str
     orchestration_instance_id: UUID = Field(alias="ORCHESTRATION-INSTANCE-ID")
     force_configuration: bool = False
@@ -68,7 +68,7 @@ class LoggingSettings(BaseSettings):
 def configure_logging(
     *,
     logging_settings: LoggingSettings,
-    extras: dict[str, Any],
+    extras: dict[str, Any] | None = None,
 ) -> None:
     """
     Configure logging to use OpenTelemetry and Azure Monitor.
