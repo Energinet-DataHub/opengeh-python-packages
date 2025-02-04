4949731eafd23327d99b2e42541ff8583b541f1a
jesper knudsen <jmk@halfspace.io>
2025-02-04 15:22:19 +0100

    fix

--- tests/unit/migrations/constants.py
+++ tests/unit/migrations/constants.py
@@ -6,3 +6,4 @@ MIGRATION_TEST_DIR = Path(__file__).parent
 TEST_SCRIPTS_DIR = "tests." + str(MIGRATION_TEST_DIR.relative_to(UNIT_TEST_DIR.parent) / "test_scripts").replace(
     "/", "."
 )
+ETL_TEST_DATA = MIGRATION_TEST_DIR / "functions" / "data"
