acb95811a466cab140e43c9d25993a3667459d98
Bjarke Meier <hej@bjarkemeier.dk>
2025-02-07 15:35:21 +0100

    a few additions

--- src/geh_common/domain/types/net_settlement_group.py
+++ src/geh_common/domain/types/net_settlement_group.py
@@ -3,3 +3,5 @@ from enum import IntEnum
 
 class NetSettlementGroup(IntEnum):
     NET_SETTLEMENT_GROUP_2 = 2
+    NET_SETTLEMENT_GROUP_3 = 3
+    NET_SETTLEMENT_GROUP_6 = 6
--- src/geh_common/domain/types/orchestration_type.py
+++ src/geh_common/domain/types/orchestration_type.py
@@ -3,3 +3,4 @@ from enum import Enum
 
 class OrchestrationType(Enum):
     ELECTRICAL_HEATING = "electrical_heating"
+    CAPACITY_SETTLEMENT = "capacity_settlement"
