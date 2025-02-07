640853e7682f930909d379b35e8a75b9e3835f93
Bjarke Meier <hej@bjarkemeier.dk>
2025-02-07 15:32:24 +0100

    ruff

--- src/geh_common/domain/types/metering_point_type.py
+++ src/geh_common/domain/types/metering_point_type.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class MeteringPointType(Enum):
     CAPACITY_SETTLEMENT = "capacity_settlement"
-    CONSUMPTION = "consumption" # Parent
+    CONSUMPTION = "consumption"  # Parent
     CONSUMPTION_FROM_GRID = "consumption_from_grid"
     CONSUMPTION_METERING_POINT_TYPE = "consumption"
     ELECTRICAL_HEATING = "electrical_heating"
@@ -13,7 +13,7 @@ class MeteringPointType(Enum):
     NET_PRODUCTION = "net_production"
     NET_TO_GRID = "net_to_grid"
     OWN_PRODUCTION = "own_production"
-    PRODUCTION = "production" # Parent
+    PRODUCTION = "production"  # Parent
     SUPPLY_TO_GRID = "supply_to_grid"
     TOTAL_CONSUMPTION = "total_consumption"
     VE_PRODUCTION = "ve_production"
