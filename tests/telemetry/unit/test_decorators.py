59f0cc78e75638fabbcc815af821b7420afd535a
chba <chba@kapacity.dk>
2025-02-20 08:28:49 +0000

    updated test_decorators with some cleaning

--- tests/telemetry/unit/test_decorators.py
+++ tests/telemetry/unit/test_decorators.py
@@ -9,6 +9,8 @@ from geh_common.telemetry.logging_configuration import (
     configure_logging,
 )
 
+from .test_logging_configuration import cleanup_logging
+
 
 # Mocking the Logger and start_span
 @pytest.fixture
@@ -89,9 +91,9 @@ def test_start_trace__when_logging_not_configured():
 def test_start_trace__when_logging_is_configured(mock_env_args):
     with (
         patch("geh_common.telemetry.decorators.Logger") as mock_logger,
-        patch("geh_common.telemetry.logging_configuration.configure_azure_monitor") as mock_configure_azure_monitor,
-    ):  # Intercepts Logger(func.__name__)
-        log_instance_in_test = mock_logger.return_value  # Intercepts log = Logger(func.__name__)
+        patch("geh_common.telemetry.logging_configuration.configure_azure_monitor"),
+    ):
+        log_instance_in_test = mock_logger.return_value
 
         # Prepare
         @start_trace()
@@ -101,7 +103,11 @@ def test_start_trace__when_logging_is_configured(mock_env_args):
 
         def entry_point():
             # Initial LoggingSettings
-            settings = LoggingSettings()
+            settings = LoggingSettings(
+                cloud_role_name="cloud_role_name",
+                applicationinsights_connection_string="connection_string",
+                subsystem="test_subsystem",
+            )
             settings.applicationinsights_connection_string = "connection_string"  # For testing purposes
 
             configure_logging(
@@ -117,9 +123,12 @@ def test_start_trace__when_logging_is_configured(mock_env_args):
             mock_logger.assert_called_once_with("app_sample_function")
             log_instance_in_test.info.assert_called_once_with("Started executing function: app_sample_function")
 
+        cleanup_logging()
+
 
 @patch("geh_common.telemetry.decorators.Logger")
 @patch("geh_common.telemetry.decorators.span_record_exception")
+@patch("geh_common.telemetry.logging_configuration.configure_azure_monitor")
 def test_logging_is_configured_error_thrown_span_records_exception(
     mock_span_record_exception,
     mock_logger,
@@ -136,7 +145,11 @@ def test_logging_is_configured_error_thrown_span_records_exception(
 
     def entry_point():
         # Initial LoggingSettings
-        settings = LoggingSettings()
+        settings = LoggingSettings(
+            cloud_role_name="cloud_role_name",
+            applicationinsights_connection_string="connection_string",
+            subsystem="test_subsystem",
+        )
         settings.applicationinsights_connection_string = "connection_string"  # For testing purposes
 
         configure_logging(
@@ -153,3 +166,5 @@ def test_logging_is_configured_error_thrown_span_records_exception(
             mock_logger.assert_called_once_with("app_sample_function")
             log_instance_in_test.info.assert_called_once_with("Started executing function: app_sample_function")
             mock_span_record_exception.assert_called_once()
+
+    cleanup_logging()
