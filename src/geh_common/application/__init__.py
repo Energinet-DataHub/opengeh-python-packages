5bb080e65d4350b6af37c4426dcba48b85d0cbbd
mrbangbricks <christian.bang@twoday.com>
2025-02-21 13:08:06 +0100

    Simplify logging setup with pydantic settings classes and decorators (#93)
    Co-authored-by: chba <chba@kapacity.dk>

---
+++ src/geh_common/application/__init__.py
@@ -0,0 +1,3 @@
+from geh_common.application import settings
+
+__all__ = [settings.__name__]  # type: ignore
--- tests/telemetry/unit/test_decorators.py
+++ tests/telemetry/unit/test_decorators.py
@@ -1,8 +1,14 @@
+from unittest import mock
 from unittest.mock import patch
 
 import pytest
 
-from geh_common.telemetry.decorators import use_span
+from geh_common.telemetry.decorators import start_trace, use_span
+from geh_common.telemetry.logging_configuration import (
+    LoggingSettings,
+    configure_logging,
+)
+from tests.telemetry.conftest import cleanup_logging
 
 
 # Mocking the Logger and start_span
@@ -16,6 +22,16 @@ def mock_logger():
 def mock_start_span():
     with patch("geh_common.telemetry.decorators.start_span") as MockStartSpan:
         yield MockStartSpan
+
+
+@pytest.fixture
+def mock_env_args():
+    env_args = {
+        "CLOUD_ROLE_NAME": "cloud_role_name from environment",
+        "APPLICATIONINSIGHTS_CONNECTION_STRING": "connection_string",
+        "SUBSYSTEM": "subsystem from environment",
+    }
+    yield env_args
 
 
 def test_use_span__when_name_is_defined(mock_logger, mock_start_span):
@@ -54,3 +70,99 @@ def test_use_span__when_name_is_not_defined(mock_logger, mock_start_span):
         "Started executing function: test_use_span__when_name_is_not_defined.<locals>.sample_function"
     )
     assert result == "test"
+
+
+def test_start_trace__when_logging_not_configured():
+    # Prepare
+    @start_trace()
+    def app_sample_function():
+        assert (1 + 1) == 2
+        return "I am an app sample function. Doing important calculations"
+
+    def entry_point():
+        app_sample_function()
+
+    # Act and assert
+    with pytest.raises(Exception):
+        entry_point()
+
+
+def test_start_trace__when_logging_is_configured(mock_env_args):
+    with (
+        patch("geh_common.telemetry.decorators.Logger") as mock_logger,
+        patch("geh_common.telemetry.logging_configuration.configure_azure_monitor"),
+    ):
+        log_instance_in_test = mock_logger.return_value
+
+        # Prepare
+        @start_trace()
+        def app_sample_function(initial_span=None):
+            assert (1 + 1) == 2
+            return "I am an app sample function. Doing important calculations"
+
+        def entry_point():
+            # Initial LoggingSettings
+            settings = LoggingSettings(
+                cloud_role_name="cloud_role_name",
+                applicationinsights_connection_string="connection_string",
+                subsystem="test_subsystem",
+            )
+            settings.applicationinsights_connection_string = "connection_string"  # For testing purposes
+
+            configure_logging(
+                logging_settings=settings,
+                extras={"key1": "value1", "key2": "value2"},
+            )
+            app_sample_function()
+
+        with mock.patch.dict("os.environ", mock_env_args, clear=False):
+            # Act
+            entry_point()
+            # Assert
+            mock_logger.assert_called_once_with("app_sample_function")
+            log_instance_in_test.info.assert_called_once_with("Started executing function: app_sample_function")
+
+        cleanup_logging()
+
+
+@patch("geh_common.telemetry.decorators.Logger")
+@patch("geh_common.telemetry.decorators.span_record_exception")
+@patch("geh_common.telemetry.logging_configuration.configure_azure_monitor")
+def test_logging_is_configured_error_thrown_span_records_exception(
+    mock_span_record_exception,
+    mock_logger,
+    mock_env_args,
+):
+    log_instance_in_test = mock_logger.return_value
+
+    # Prepare
+    @start_trace()
+    def app_sample_function():
+        raise Exception  # Mimmic an raised exception during runtime
+        return "I am an app sample function. Doing important calculations"
+
+    def entry_point():
+        # Initial LoggingSettings
+        settings = LoggingSettings(
+            cloud_role_name="cloud_role_name",
+            applicationinsights_connection_string="connection_string",
+            subsystem="test_subsystem",
+        )
+        settings.applicationinsights_connection_string = "connection_string"  # For testing purposes
+
+        configure_logging(
+            logging_settings=settings,
+            extras={"key1": "value1", "key2": "value2"},
+        )
+        app_sample_function()
+
+    # Mimic machine setting environment variables
+    with mock.patch.dict("os.environ", mock_env_args, clear=False):
+        with pytest.raises(SystemExit):
+            entry_point()
+            # Assert
+            mock_logger.assert_called_once_with("app_sample_function")
+            log_instance_in_test.info.assert_called_once_with("Started executing function: app_sample_function")
+            mock_span_record_exception.assert_called_once()
+
+    cleanup_logging()
