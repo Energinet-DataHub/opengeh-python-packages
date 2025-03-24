09ab4448c17d5d2bb82dab99aae06ec02044c81e
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2025-03-24 13:00:08 +0100

    Extend orchestration types enum and update version (#133)

--- source/geh_common/src/geh_common/domain/types/orchestration_type.py
+++ source/geh_common/src/geh_common/domain/types/orchestration_type.py
@@ -3,6 +3,17 @@ from enum import Enum
 
 class OrchestrationType(Enum):
     ELECTRICAL_HEATING = "electrical_heating"
+    """Process for calculation of electrical heating for households with electrical heating."""
     CAPACITY_SETTLEMENT = "capacity_settlement"
+    """Process for calculating the capacity settlement for the largest consumers on the grid."""
     SUBMITTED = "submitted"
+    """Process for submitting measurements from the process manager to measurements core."""
     MIGRATION = "migration"
+    """Process for transferring measurements from subsystem 'datamigrations' to measurements core.
+    The measurements from 'datamigrations' are migrated from DataHub 2."""
+    NET_CONSUMPTION = "net_consumption"
+    """Process that calculates the net consumption for net settlement group 6 households."""
+    MISSING_MEASUREMENTS_LOG = "missing_measurements_log"
+    """Process for calculating the missing metering points measurements in all grid areas."""
+    MISSING_MEASUREMENTS_LOG_ON_DEMAND = "missing_measurements_log_on_demand"
+    """Process for calculating the missing metering points measurements in selected grid areas."""
