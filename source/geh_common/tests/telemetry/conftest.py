cfec6ebeb77d6e9487c11efc8ffb3b69d0f40d92
Søren Frøhlich Kronborg <sorenfrohlichkronborg@Srens-MacBook-Pro.local>
2025-03-31 12:51:55 +0200

    added logging changes and fixed unit test, still missing int tests

--- source/geh_common/tests/telemetry/conftest.py
+++ source/geh_common/tests/telemetry/conftest.py
@@ -1,10 +1,10 @@
 import os
+import sys
 from unittest import mock
 
 import pytest
 
 from geh_common.telemetry.logging_configuration import (
-    LoggingSettings,
     configure_logging,
     set_extras,
     set_is_instrumented,
@@ -15,7 +15,6 @@ from geh_common.telemetry.logging_configuration import (
 UNIT_TEST_CLOUD_ROLE_NAME = "test_role"
 UNIT_TEST_SUBSYSTEM = "test_subsystem"
 UNIT_TEST_DUMMY_CONNECTION_STRING = "connectionString"
-UNIT_TEST_SYS_ARGS = ["program_name", "--orchestration-instance-id", "4a540892-2c0a-46a9-9257-c4e13051d76a"]
 
 
 def cleanup_logging() -> None:
@@ -32,19 +31,19 @@ def unit_logging_configuration_with_connection_string():
     Fixture to setup the logging configuration used for unit tests
     Fixture sets up the logging, but patches configure_azure_monitor so it will not try to actually configure a real connection
     """
-    sys_args = UNIT_TEST_SYS_ARGS
+    orchestration_instance_id = "4a540892-2c0a-46a9-9257-c4e13051d76a"
+    sys_args = ["program_name", "--orchestration-instance-id", orchestration_instance_id]
 
     # Command line arguments
-    with (
-        mock.patch("sys.argv", sys_args),
-        mock.patch("geh_common.telemetry.logging_configuration.configure_azure_monitor"),
-    ):
-        logging_settings = LoggingSettings(
-            cloud_role_name=UNIT_TEST_CLOUD_ROLE_NAME,
-            subsystem=UNIT_TEST_SUBSYSTEM,
-            applicationinsights_connection_string=UNIT_TEST_DUMMY_CONNECTION_STRING,
+    with pytest.MonkeyPatch.context() as ctx:
+        ctx.setenv("APPLICATIONINSIGHTS_CONNECTION_STRING", UNIT_TEST_DUMMY_CONNECTION_STRING)
+        ctx.setattr(sys, "argv", sys_args)
+        ctx.setattr("geh_common.telemetry.logging_configuration.configure_azure_monitor", mock.Mock())
+
+        yield (
+            configure_logging(cloud_role_name=UNIT_TEST_CLOUD_ROLE_NAME, subsystem=UNIT_TEST_SUBSYSTEM),
+            orchestration_instance_id,
         )
-        yield configure_logging(logging_settings=logging_settings), logging_settings
 
     # Clean up logging configuration module after each usage of the fixture, by setting logging configured to False
     cleanup_logging()
@@ -57,21 +56,20 @@ def unit_logging_configuration_with_connection_string_with_extras():
     Fixture sets up the logging, but patches configure_azure_monitor so it will not try to actually configure a real connection
     """
     initial_extras = {"extra_key": "extra_value"}
-    sys_args = UNIT_TEST_SYS_ARGS
+    orchestration_instance_id = "4a540892-2c0a-46a9-9257-c4e13051d76a"
+    sys_args = ["program_name", "--orchestration-instance-id", orchestration_instance_id]
 
     # Command line arguments
-    with (
-        mock.patch("sys.argv", sys_args),
-        mock.patch("geh_common.telemetry.logging_configuration.configure_azure_monitor"),
-    ):
-        logging_settings = LoggingSettings(
-            cloud_role_name=UNIT_TEST_CLOUD_ROLE_NAME,
-            subsystem=UNIT_TEST_SUBSYSTEM,
-            applicationinsights_connection_string=UNIT_TEST_DUMMY_CONNECTION_STRING,
-        )
+    with pytest.MonkeyPatch.context() as ctx:
+        ctx.setenv("APPLICATIONINSIGHTS_CONNECTION_STRING", UNIT_TEST_DUMMY_CONNECTION_STRING)
+        ctx.setattr(sys, "argv", sys_args)
+        ctx.setattr("geh_common.telemetry.logging_configuration.configure_azure_monitor", mock.Mock())
+
         yield (
-            configure_logging(logging_settings=logging_settings, extras=initial_extras),
-            logging_settings,
+            configure_logging(
+                cloud_role_name=UNIT_TEST_CLOUD_ROLE_NAME, subsystem=UNIT_TEST_SUBSYSTEM, extras=initial_extras
+            ),
+            orchestration_instance_id,
             initial_extras,
         )
 
