5bac512b0c8786374a8923997b08cac2d6c6bbee
Søren Frøhlich Kronborg <sorenfrohlichkronborg@Srens-MacBook-Pro.local>
2025-03-31 15:39:00 +0200

    test integration

--- source/geh_common/tests/telemetry/integration/test_integration_azure_monitor.py
+++ source/geh_common/tests/telemetry/integration/test_integration_azure_monitor.py
@@ -29,48 +29,44 @@ def fixture_logger():
 
 @pytest.fixture
 def integration_logging_configuration_setup(integration_test_configuration):
-    orchestration_instance_id = str(uuid.uuid4())
-    unique_cloud_role_name = INTEGRATION_TEST_CLOUD_ROLE_NAME + "_" + orchestration_instance_id
-    sys_args = ["program_name", "--orchestration-instance-id", orchestration_instance_id]
-    connection_string = integration_test_configuration.get_applicationinsights_connection_string()
+    orchestration_instance_id = uuid.uuid4()
+    unique_cloud_role_name = INTEGRATION_TEST_CLOUD_ROLE_NAME + "_" + str(orchestration_instance_id)
+    sys_args = ["program_name", "--orchestration-instance-id", str(orchestration_instance_id)]
 
     # Command line arguments
     with pytest.MonkeyPatch.context() as ctx:
-        ctx.setenv("APPLICATIONINSIGHTS_CONNECTION_STRING", connection_string)
-        ctx.setattr(sys, "argv", sys_args)
-
-        # Remove any previously attached log handlers. Without it, handlers from previous tests can accumulate, causing multiple log messages for each event.
-        logging.getLogger().handlers.clear()
-
-        logging_settings = configure_logging(
-            cloud_role_name=unique_cloud_role_name,
-            subsystem=SUBSYSTEM,
-        )
-        logging.getLogger().handlers.clear()
-        yield logging_settings, orchestration_instance_id
-        cleanup_logging()
+            ctx.setattr(sys, "argv", sys_args)
+            ctx.setenv(
+                "APPLICATIONINSIGHTS_CONNECTION_STRING",
+                integration_test_configuration.get_applicationinsights_connection_string(),
+            )
+            # Remove any previously attached log handlers. Without it, handlers from previous tests can accumulate, causing multiple log messages for each event.
+            logging.getLogger().handlers.clear()
+            logging_configurations = configure_logging(subsystem=SUBSYSTEM, cloud_role_name=unique_cloud_role_name)
+            yield logging_configurations, unique_cloud_role_name
+            cleanup_logging()
 
 
 @pytest.fixture(scope="function")
 def integration_logging_configuration_setup_with_extras(integration_test_configuration):
     key = "key"
     extras = {key: "value"}
-    orchestration_instance_id = str(uuid.uuid4())
-    unique_cloud_role_name = INTEGRATION_TEST_CLOUD_ROLE_NAME + "_" + orchestration_instance_id
-    connection_string = integration_test_configuration.get_applicationinsights_connection_string()
-    sys_args = ["program_name", "--orchestration-instance-id", orchestration_instance_id]
+    new_uuid = uuid.uuid4()
+    sys_argv = ["dummy_script_name", "--orchestration-instance-id", str(new_uuid)]
+    unique_cloud_role_name = INTEGRATION_TEST_CLOUD_ROLE_NAME + "_" + str(new_uuid)
 
     with pytest.MonkeyPatch.context() as ctx:
-        ctx.setenv("APPLICATIONINSIGHTS_CONNECTION_STRING", connection_string)
-        ctx.setattr(sys, "argv", sys_args)
+        ctx.setattr(sys, "argv", sys_argv)
+        ctx.setenv(
+            "APPLICATIONINSIGHTS_CONNECTION_STRING",
+            integration_test_configuration.get_applicationinsights_connection_string(),
+        )
         # Remove any previously attached log handlers. Without it, handlers from previous tests can accumulate, causing multiple log messages for each event.
         logging.getLogger().handlers.clear()
-
-        logging_settings = configure_logging(
-            cloud_role_name=unique_cloud_role_name,
-            subsystem=SUBSYSTEM,
-        )
-        yield logging_settings, orchestration_instance_id, extras
+        yield (
+            configure_logging(cloud_role_name=unique_cloud_role_name, subsystem=SUBSYSTEM, extras=extras),
+            extras,
+        )  # 2nd par beforelogging_settings, extras
         cleanup_logging()
 
 
