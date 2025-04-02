8c1375d575d0251bc90db99afe53df2defa8c329
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-02 12:06:32 +0200

    doc: Add contracts for data products in calculated measurements

---
+++ source/geh_common/src/geh_common/data_products/measurements_calculated/hourly_calculated_measurements_v1.py
@@ -0,0 +1,49 @@
+import pyspark.sql.types as t
+
+nullable = True
+
+# Schema name
+schema = "measurements_calculated"
+
+# View name
+hourly_calculated_measurements_v1 = t.StructType(
+    [
+        # "electrical_heating" | "capacity_settlement" | "net_consumption"
+        t.StructField("orchestration_type", t.StringType(), not nullable),
+        #
+        # ID of the orchestration that initiated the calculation job
+        t.StructField("orchestration_instance_id", t.StringType(), not nullable),
+        #
+        # Transaction ID created by the calculation job. The ID refers to a continous set of measurements for a
+        # specific combination of orchestration_id and metering_point_id. There are no time gaps for a given
+        # transaction id. Gaps introduces a new transaction ID after the gap.
+        t.StructField("transaction_id", t.StringType(), not nullable),
+        #
+        # A datetime value indicating when the transaction was created by the calculation job.
+        t.StructField("transaction_creation_datetime", t.TimestampType(), not nullable),
+        #
+        # GSRN number
+        t.StructField("metering_point_id", t.StringType(), not nullable),
+        #
+        # 'electrical_heating' | 'capacity_settlement' | 'net_consumption'
+        t.StructField("metering_point_type", t.StringType(), not nullable),
+        #
+        # UTC datetime
+        t.StructField("observation_time", t.TimestampType(), not nullable),
+        #
+        # The calculated quantity
+        t.StructField("quantity", t.DecimalType(18, 3), not nullable),
+        #
+        # The unit of the calculated quantity
+        # "kWh"
+        t.StructField("quantity_unit", t.StringType(), not nullable),
+        #
+        # The quality of the calculated quantity
+        # "calculated"
+        t.StructField("quantity_quality", t.StringType(), not nullable),
+        #
+        # The resolution of the calculated quantity
+        # "PT1H"
+        t.StructField("resolution", t.StringType(), not nullable),
+    ]
+)
