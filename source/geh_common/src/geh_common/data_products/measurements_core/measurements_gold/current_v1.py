07580ac42f53a185a57f9357d067df0cec77c787
Jan Duelund <jdd@energinet.dk>
2025-04-08 13:31:38 +0200

    fix: current_v1 contract is now not nullable (#145)

--- source/geh_common/src/geh_common/data_products/measurements_core/measurements_gold/current_v1.py
+++ source/geh_common/src/geh_common/data_products/measurements_core/measurements_gold/current_v1.py
@@ -9,10 +9,10 @@ schema = "measurements_gold"
 current_v1 = t.StructType(
     # Fields are nullable, but we are checking for nulls in the view
     [
-        t.StructField("metering_point_id", t.StringType(), nullable),
-        t.StructField("observation_time", t.TimestampType(), nullable),
-        t.StructField("quantity", t.DecimalType(18, 3), nullable),
-        t.StructField("quality", t.StringType(), nullable),
-        t.StructField("metering_point_type", t.StringType(), nullable),
+        t.StructField("metering_point_id", t.StringType(), not nullable),
+        t.StructField("observation_time", t.TimestampType(), not nullable),
+        t.StructField("quantity", t.DecimalType(18, 3), not nullable),
+        t.StructField("quality", t.StringType(), not nullable),
+        t.StructField("metering_point_type", t.StringType(), not nullable),
     ]
 )
