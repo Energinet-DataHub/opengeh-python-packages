5406dca002f62889d1da07b54e7cc6ba944c4e3f
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-09 09:22:37 +0200

    Update electrical_heating_consumption_metering_point_periods_v1.py

--- source/geh_common/src/geh_common/data_products/electricity_market_measurements_input/electrical_heating_consumption_metering_point_periods_v1.py
+++ source/geh_common/src/geh_common/data_products/electricity_market_measurements_input/electrical_heating_consumption_metering_point_periods_v1.py
@@ -42,30 +42,30 @@ electrical_heating_consumption_metering_point_periods_v1 = T.StructType(
     ]
 )
 """
-    Consumption (parent) metering points related to electrical heating.
-    The data is periodized; the following transaction types are relevant for determining the periods:
-    - CHANGESUP: Leverandørskift (BRS-001)
-    - ENDSUPPLY: Leveranceophør (BRS-002)
-    - INCCHGSUP: Håndtering af fejlagtigt leverandørskift (BRS-003)
-    - MSTDATSBM: Fremsendelse af stamdata (BRS-006) - Skift af nettoafregningsgrupper
-    - LNKCHLDMP: Tilkobling af D15 til parent i nettoafregningsgruppe 2
-    - ULNKCHLDMP: Afkobling af D15 af parent i nettoafregningsgruppe 2
-    - ULNKCHLDMP: Afkobling af D14 af parent
-    - MOVEINES: Tilflytning - meldt til elleverandøren (BRS-009)
-    - MOVEOUTES: Fraflytning - meldt til elleverandøren (BRS-010)
-    - INCMOVEAUT: Fejlagtig flytning - Automatisk (BRS-011)
-    - INCMOVEMAN: Fejlagtig flytning - Manuel (BRS-011) HTX
-    - MDCNSEHON: Oprettelse af elvarme (BRS-015) Det bliver til BRS-041 i DH3
-    - MDCNSEHOFF: Fjernelse af elvarme (BRS-015) Det bliver til BRS-041 i DH3
-    - CHGSUPSHRT: Leverandørskift med kort varsel (BRS-043). Findes ikke i DH3
-    - MANCHGSUP: Tvunget leverandørskifte på målepunkt (BRS-044).
-    - MANCOR (HTX): Manuelt korrigering
-    Periods are  included when
-    - the metering point physical status is connected or disconnected
-    - the period does not end before 2021-01-01
-    - the electrical heating is or has been registered for the period
+Consumption (parent) metering points related to electrical heating.
+The data is periodized; the following transaction types are relevant for determining the periods:
+- CHANGESUP: Leverandørskift (BRS-001)
+- ENDSUPPLY: Leveranceophør (BRS-002)
+- INCCHGSUP: Håndtering af fejlagtigt leverandørskift (BRS-003)
+- MSTDATSBM: Fremsendelse af stamdata (BRS-006) - Skift af nettoafregningsgrupper
+- LNKCHLDMP: Tilkobling af D15 til parent i nettoafregningsgruppe 2
+- ULNKCHLDMP: Afkobling af D15 af parent i nettoafregningsgruppe 2
+- ULNKCHLDMP: Afkobling af D14 af parent
+- MOVEINES: Tilflytning - meldt til elleverandøren (BRS-009)
+- MOVEOUTES: Fraflytning - meldt til elleverandøren (BRS-010)
+- INCMOVEAUT: Fejlagtig flytning - Automatisk (BRS-011)
+- INCMOVEMAN: Fejlagtig flytning - Manuel (BRS-011) HTX
+- MDCNSEHON: Oprettelse af elvarme (BRS-015) Det bliver til BRS-041 i DH3
+- MDCNSEHOFF: Fjernelse af elvarme (BRS-015) Det bliver til BRS-041 i DH3
+- CHGSUPSHRT: Leverandørskift med kort varsel (BRS-043). Findes ikke i DH3
+- MANCHGSUP: Tvunget leverandørskifte på målepunkt (BRS-044).
+- MANCOR (HTX): Manuelt korrigering
+Periods are  included when
+- the metering point physical status is connected or disconnected
+- the period does not end before 2021-01-01
+- the electrical heating is or has been registered for the period
 
-    Formatting is according to ADR-144 with the following constraints:
-    - No column may use quoted values
-    - All date/time values must include seconds
-    """
+Formatting is according to ADR-144 with the following constraints:
+- No column may use quoted values
+- All date/time values must include seconds
+"""
