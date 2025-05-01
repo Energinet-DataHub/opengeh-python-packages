b0f406c87d26b941ef8987911aa04297055a020f
Soren <XSSFR@energinet.dk>
2025-05-01 11:27:06 +0200

    testing new integration test

---
+++ source/geh_common/tests/databricks/integration/test_databricks_api_client.py
@@ -0,0 +1,14 @@
+import os
+
+
+def test__new_env_variables():
+    # Arrange
+    databricks_token = os.getenv("DATABRICKS_TOKEN")
+    databricks_host = os.getenv("DATABRICKS_HOST")
+    databricks_warehouse_id = os.getenv("DATABRICKS_WAREHOUSE_ID")
+
+    assert databricks_token is not None, "DATABRICKS_TOKEN is not set"
+    assert databricks_host is not None, "DATABRICKS_HOST is not set"
+    assert databricks_warehouse_id is not None, "DATABRICKS_WAREHOUSE_ID is not set"
+
+
