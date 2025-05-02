2035bc895834958e2562e7847a9eab5239150bc5
Soren <XSSFR@energinet.dk>
2025-05-02 10:51:24 +0000

    test if env variables are set correct

--- source/geh_common/tests/databricks/integration/test_integration_databricks_api_client.py
+++ source/geh_common/tests/databricks/integration/test_integration_databricks_api_client.py
@@ -4,6 +4,10 @@ from geh_common.databricks.databricks_api_client import DatabricksApiClient
 
 
 def test__execute_statement_returns_correct_state_and_data():
+    """
+    Test that the execute_statement method returns the correct state and data.
+    """
+
     # Arrange
     databricks_token = os.getenv("DATABRICKS_TOKEN")
     databricks_host = os.getenv("DATABRICKS_HOST")
