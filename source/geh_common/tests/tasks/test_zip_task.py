ad24712c7d2982198bade2a8e4968b35dcbc2e21
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-19 10:28:41 +0200

    test: tests that dbutils mock works as expected
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_common/tests/tasks/test_zip_task.py
+++ source/geh_common/tests/tasks/test_zip_task.py
@@ -1,3 +1,6 @@
+import shutil
+from pathlib import Path
+
 import pytest
 
 from geh_common.tasks.ZipTask import ZipTask
@@ -13,7 +16,7 @@ def mock_dbutils(monkeypatch):
         def fs(self):
             class MockFS:
                 def ls(self, path):
-                    return [MockFileInfo("file1.txt"), MockFileInfo("file2.txt")]
+                    return [MockFileInfo(f.name) for f in Path(path).iterdir()]
 
             return MockFS()
 
@@ -27,3 +30,23 @@ def test_init_zip_task(spark, mock_dbutils):
     assert task.output_path == "/tmp/test"
     assert task.zip_output_path == "/tmp/test.zip"
     assert task.spark == spark
+
+
+def test_dbutils_mocked(spark, tmp_path_factory, mock_dbutils):
+    # Arrange
+    tmp_path: Path = tmp_path_factory.mktemp("test_zip_task")
+    task = ZipTask(spark, tmp_path)
+    assert task.dbutils.fs().ls(tmp_path) == []
+    assert isinstance(task.dbutils, mock_dbutils.__class__)
+    for i in range(3):
+        (tmp_path / f"file_{i}.txt").write_text(f"Content of file {i}")
+
+    # Act
+    files: list[Path] = task.dbutils.fs().ls(str(tmp_path))
+
+    # Assert
+    assert len(files) == 3
+    assert sorted([f.name for f in files]) == sorted([f"file_{i}.txt" for i in range(3)])
+
+    # Clean up
+    shutil.rmtree(tmp_path)
