4f6c60690e3fc0c26d2df9e585d89a1b77bdc49a
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-20 16:01:27 +0200

    chore: move files correct modules
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_common/src/geh_common/infrastructure/write_csv.py
+++ source/geh_common/src/geh_common/infrastructure/write_csv.py
@@ -1,9 +1,8 @@
 import random
 import string
-import zipfile
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Callable
+from typing import Callable
 
 from pyspark.sql import DataFrame, Window
 from pyspark.sql import functions as F
@@ -44,43 +43,6 @@ class FileInfo:
     source: Path
     destination: Path
     temporary: Path
-
-
-def create_zip_file(path: str | Path, dbutils: Any, tmpdir: str | Path = Path("tmp")) -> Path:
-    """Create a zip file from a list of files and saves it to the specified path.
-
-    Notice that we have to create the zip file in /tmp and then move it to the desired
-    location. This is done as `direct-append` or `non-sequential` writes are not
-    supported in Databricks.
-
-    Args:
-        path (str | Path): The path to the directory containing the files to zip.
-        dbutils: The Databricks utilities object.
-        tmpdir (str | Path, optional): The temporary directory to write the zip file to. Defaults to "tmp".
-
-    Raises:
-        Exception: If there are no files to zip.
-
-    Returns:
-        Path: The path to the created zip file.
-    """
-    output_path = Path(path)
-    if not output_path.exists():
-        raise ValueError(f"'{output_path}' does not exist")
-    if not output_path.is_dir():
-        raise ValueError(f"'{output_path}' is not a directory")
-    zip_output_path = output_path.with_suffix(".zip")
-    files_to_zip = [Path(f) for f in dbutils.fs.ls(str(output_path))]
-    if len(files_to_zip) == 0:
-        raise FileNotFoundError(f"No files found in {output_path}")
-    tmp_path = Path(tmpdir) / zip_output_path.name
-    Path(tmp_path).parent.mkdir(parents=True, exist_ok=True)
-    with zipfile.ZipFile(tmp_path, "w", zipfile.ZIP_DEFLATED) as ref:
-        for fp in files_to_zip:
-            file_name = fp.name
-            ref.write(fp, arcname=file_name)
-    dbutils.fs.mv(f"file:{tmp_path}", zip_output_path)
-    return zip_output_path
 
 
 def write_csv_files(
@@ -133,11 +95,34 @@ def write_csv_files(
     return content
 
 
+def get_partition_information(path) -> dict[str, str]:
+    """Extract partition information from a file path.
+
+    Args:
+        path (str or Path): The file path from which to extract partition information.
+            The path should contain partition information in the format "key=value".
+            For example, "/tmp/part=1/part2=2/test" would yield {"part": "1", "part2": "2"}.
+
+    Raises:
+        ValueError: If the path contains an invalid partition format (e.g., "key=value=value").
+            This error occurs when there are too many values to unpack from the partition string.
+
+    Returns:
+        dict[str, str]: A dictionary with partition names as keys and their values.
+    """
+    partitions = {}
+    for p in Path(path).parts:
+        if "=" in p:
+            key, value = p.split("=", 2)
+            partitions[key] = value
+    return partitions
+
+
 def _get_file_info(
     result_output_path: str | Path,
     spark_output_path: str | Path,
     tmpdir: str | Path,
-    file_name_factory: FileNameCallbackType = _default_file_name_callback,
+    file_name_factory: FileNameCallbackType,
 ) -> list[FileInfo]:
     """Get file information for the files to be zipped.
 
@@ -258,26 +243,3 @@ def _merge_content(file_info: list[FileInfo], headers: list[str]) -> list[Path]:
                     fh_destination.write(fh_temporary.read())
 
     return list(destinations.keys())
-
-
-def get_partition_information(path) -> dict[str, str]:
-    """Extract partition information from a file path.
-
-    Args:
-        path (str or Path): The file path from which to extract partition information.
-            The path should contain partition information in the format "key=value".
-            For example, "/tmp/part=1/part2=2/test" would yield {"part": "1", "part2": "2"}.
-
-    Raises:
-        ValueError: If the path contains an invalid partition format (e.g., "key=value=value").
-            This error occurs when there are too many values to unpack from the partition string.
-
-    Returns:
-        dict[str, str]: A dictionary with partition names as keys and their values.
-    """
-    partitions = {}
-    for p in Path(path).parts:
-        if "=" in p:
-            key, value = p.split("=", 2)
-            partitions[key] = value
-    return partitions
--- source/geh_common/tests/infrastructure/test_write_csv.py
+++ source/geh_common/tests/infrastructure/test_write_csv.py
@@ -1,48 +1,16 @@
 import shutil
-import zipfile
 from datetime import datetime, timezone
 from pathlib import Path
 
 import pytest
 from pyspark.sql import SparkSession
 
-from geh_common.tasks.ZipTask import (
+from geh_common.infrastructure.write_csv import (
     CHUNK_INDEX_COLUMN,
     _write_dataframe,
-    create_zip_file,
     get_partition_information,
     write_csv_files,
 )
-from geh_common.testing.spark.mocks import MockDBUtils
-
-
-def test_zip_dir(tmp_path_factory):
-    # Arrange
-    outputdir = tmp_path_factory.mktemp("test_zip_task")
-    tmpdir = tmp_path_factory.mktemp("tmp_dir")
-    files = ["file_1.txt", "file_2.txt", "file_3.txt"]
-    for file in files:
-        (outputdir / file).write_text(f"Content of {file}")
-
-    # Act
-    zip_file = create_zip_file(outputdir, MockDBUtils(), tmpdir)
-
-    # Assert
-    assert zip_file == Path(outputdir).with_suffix(".zip"), (
-        f"Expected {zip_file} to be {Path(outputdir).with_suffix('.zip')}"
-    )
-    assert zip_file.exists(), f"Zip file {zip_file} does not exist"
-    assert zip_file.stat().st_size > 0, f"Zip file {zip_file} is empty"
-
-    with zipfile.ZipFile(zip_file, "r") as zf:
-        zip_files = zf.namelist()
-        assert len(zip_files) == len(files), f"Expected {len(files)} files in zip, but got {len(zip_files)}"
-        for file in files:
-            assert file in zip_files, f"File {file} not found in zip"
-
-    # Clean up
-    shutil.rmtree(outputdir)
-    shutil.rmtree(tmpdir)
 
 
 def test_zip_task_write_files_default(spark):
