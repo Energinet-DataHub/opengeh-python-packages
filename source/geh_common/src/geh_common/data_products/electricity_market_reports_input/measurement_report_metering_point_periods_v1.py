19ea02b644672f94dca5218d4349a5ba88441228
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-27 10:50:49 +0200

    feat: adds data contract description for measurement_report_metering_point_periods_v1
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

---
+++ source/geh_common/src/geh_common/data_products/electricity_market_reports_input/measurement_report_metering_point_periods_v1.py
@@ -0,0 +1,27 @@
+import pyspark.sql.types as T
+
+nullable = True
+
+database_name = "electricity_market_reports_input"
+
+view_name = "measurement_report_metering_point_periods_v1"
+
+schema = T.StructType(
+    [
+        T.StructField("grid_area_code", T.StringType(), not nullable),
+        T.StructField("metering_point_id", T.StringType(), not nullable),
+        T.StructField("metering_point_type", T.StringType(), not nullable),
+        T.StructField("metering_point_resolution", T.StringType(), not nullable),
+        T.StructField("energy_supplier_id", T.StringType(), nullable),
+        T.StructField("physical_status", T.BooleanType(), not nullable),
+        T.StructField("settlement_method", T.StringType(), nullable),
+        T.StructField("unit", T.StringType(), not nullable),
+        T.StructField("from_grid_area_code", T.StringType(), nullable),
+        T.StructField("to_grid_area_code", T.StringType(), nullable),
+        T.StructField("period_from_datetime", T.TimestampType(), not nullable),
+        T.StructField("period_to_datetime", T.TimestampType(), nullable),
+    ]
+)
+"""
+Child metering points related to measurement reports.
+"""
