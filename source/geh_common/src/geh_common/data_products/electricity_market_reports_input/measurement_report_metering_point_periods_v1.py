7e1036958bd03e4ddac6aa455467b98bb4bc6196
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-05-27 15:41:03 +0200

    Update measurement_report_metering_point_periods_v1.py

--- source/geh_common/src/geh_common/data_products/electricity_market_reports_input/measurement_report_metering_point_periods_v1.py
+++ source/geh_common/src/geh_common/data_products/electricity_market_reports_input/measurement_report_metering_point_periods_v1.py
@@ -8,20 +8,44 @@ view_name = "measurements_report_metering_point_periods_v1"
 
 schema = T.StructType(
     [
+
         T.StructField("grid_area_code", T.StringType(), not nullable),
+        #
+        # GSRN number
         T.StructField("metering_point_id", T.StringType(), not nullable),
+        #
+        # All types of metering point types
         T.StructField("metering_point_type", T.StringType(), not nullable),
+        #
+        # 'P15M' | 'PT1H'
         T.StructField("resolution", T.StringType(), not nullable),
+        #
+        # GLN or EIC number of the energy supplier
         T.StructField("energy_supplier_id", T.StringType(), nullable),
+        #
+        # 'connected' or 'disconnected'
         T.StructField("physical_status", T.BooleanType(), not nullable),
-        T.StructField("settlement_method", T.StringType(), nullable),
+        #
+        # The unit of the quantity (e.g. "kWh", "kVArh" etc.)
         T.StructField("quantity_unit", T.StringType(), not nullable),
+        #
+        # Required for exchange, otherwise null
         T.StructField("from_grid_area_code", T.StringType(), nullable),
+        #
+        # Required for exchange, otherwise null
         T.StructField("to_grid_area_code", T.StringType(), nullable),
-        T.StructField("period_from_datetime", T.TimestampType(), not nullable),
-        T.StructField("period_to_datetime", T.TimestampType(), nullable),
+        #
+        # See the description of periodization of data above.
+        # UTC time
+        T.StructField("period_from_date", T.TimestampType(), not nullable),
+        #
+        # See the description of periodization of data above.
+        # UTC time
+        T.StructField("period_to_date", T.TimestampType(), nullable),
     ]
 )
 """
-Child metering points related to measurement reports.
+Metering point periods used for generation of measurements reports.
+
+Includes all metering points types but only those that are connected or disconnected.
 """
