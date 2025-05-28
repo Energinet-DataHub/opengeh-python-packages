eef0c2b48518b795cd6a07e9d74be5d6c9c4739a
Jan Duelund <jdd@energinet.dk>
2025-05-28 07:25:02 +0200

    added view

--- source/geh_common/src/geh_common/data_products/measurements_core/measurements_gold/current_v1.py
+++ source/geh_common/src/geh_common/data_products/measurements_core/measurements_gold/current_v1.py
@@ -7,11 +7,11 @@ database_name = "measurements_gold"
 view_name = "current_v1"
 
 schema = t.StructType(
-    # Fields are nullable, but we are checking for nulls in the view
+    # Fields are nullable, but we are checking for nulls in the view, except for quantity
     [
         t.StructField("metering_point_id", t.StringType(), not nullable),
         t.StructField("observation_time", t.TimestampType(), not nullable),
-        t.StructField("quantity", t.DecimalType(18, 3), not nullable),
+        t.StructField("quantity", t.DecimalType(18, 3), nullable),
         t.StructField("quality", t.StringType(), not nullable),
         t.StructField("metering_point_type", t.StringType(), not nullable),
     ]
