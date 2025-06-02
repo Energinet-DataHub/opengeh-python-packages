bb7b5df04c81049361ece2471a767b0bd0ee4506
Jens Peder Meldgaard <jpm@halfspace.io>
2025-06-02 10:25:20 +0200

    chore: move energy supplier validation
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

---
+++ source/geh_common/src/geh_common/application/types/energy_supplier_ids.py
@@ -0,0 +1,39 @@
+import re
+from typing import Annotated, Any
+
+from pydantic import AfterValidator, BeforeValidator
+from pydantic_settings import NoDecode
+
+
+def _str_to_list(value: Any) -> list[str] | None:
+    """Convert the input value to a list of grid area codes (strings).
+
+    Args:
+        value (Any): The input value to convert.
+
+    Returns:
+        Optional[List[str]]: A list of grid area codes or None if the input is empty.
+    """
+    if not value:
+        return None
+    if isinstance(value, list):
+        return [str(item) for item in value]
+    else:
+        return re.findall(r"\d+", value)
+
+
+def _validate_energy_supplier_ids(v: list[str]) -> list[str]:
+    """Validate the list of energy supplier IDs."""
+    if not v:
+        return v
+    for id_ in v:
+        if not isinstance(id_, str):
+            raise TypeError(f"Energy supplier IDs must be strings, not {type(id_)}")
+        if not id_.isdigit():
+            raise ValueError(f"Unexpected energy supplier ID: '{id_}'.")
+    return v
+
+
+EnergySupplierIds = Annotated[
+    list[str] | None, BeforeValidator(_str_to_list), AfterValidator(_validate_energy_supplier_ids), NoDecode()
+]
