b7517cbb0c7f3e836290f0f7cfa83df01d165a0e
Jens Peder Meldgaard <jpm@halfspace.io>
2025-06-02 10:28:41 +0200

    chore: export validator from parent
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_common/src/geh_common/application/__init__.py
+++ source/geh_common/src/geh_common/application/__init__.py
@@ -1,4 +1,5 @@
 from geh_common.application import settings
+from geh_common.application.types.energy_supplier_ids import EnergySupplierIds
 from geh_common.application.types.grid_area_codes import GridAreaCodes
 
-__all__ = ["settings", "GridAreaCodes"]
+__all__ = ["settings", "GridAreaCodes", "EnergySupplierIds"]
