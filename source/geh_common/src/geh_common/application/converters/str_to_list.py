a0ce7f8bccd1dcc556bd167ea6d4e91d8dcee16e
Jens Peder Meldgaard <jpm@halfspace.io>
2025-06-02 13:57:36 +0200

    style: adds comments
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_common/src/geh_common/application/converters/str_to_list.py
+++ source/geh_common/src/geh_common/application/converters/str_to_list.py
@@ -24,27 +24,21 @@ def str_to_list(value: Any) -> list[str] | None:
 def _convert_values(items) -> list[str] | None:
     valid_values = []
     for item in items:
-        normalized = _normalize_value(item)
+        normalized = None
+        if isinstance(item, int | str):
+            normalized = _normalize_scalar_value(item)
+        elif isinstance(item, list):
+            normalized = _convert_values(item)
+
+        # Only add non-empty normalized values
         if normalized is not None:
             valid_values.extend(normalized)
+
+    # If no valid values were found, return None
     if not valid_values:
         return None
+
     return valid_values
-
-
-def _normalize_value(value: Any) -> list[str] | None:
-    """Normalize a value to a string or None.
-
-    Args:
-        value (Any): The input value to normalize.
-
-    Returns:
-        Optional[str]: The normalized string or None if the input is empty.
-    """
-    if isinstance(value, int | str):
-        return _normalize_scalar_value(value)
-    elif isinstance(value, list):
-        return _convert_values(value)
 
 
 def _normalize_scalar_value(value: Any) -> list[str] | None:
@@ -54,12 +48,12 @@ def _normalize_scalar_value(value: Any) -> list[str] | None:
         value (Any): The input value to normalize.
 
     Returns:
-        Optional[str]: The normalized string or None if the input is empty.
+        Optional[List[str]]: A list containing the normalized string or None if the input is empty.
     """
     item = str(value)
     if item.startswith("[") and item.endswith("]"):
         item = item[1:-1]
     item_normalized = str(item).strip()
-    if item_normalized and item_normalized != "[]":
+    if item_normalized:
         return item_normalized.split(",")
     return None
