6d7741faf70250139a7082eeaf783bdc62242661
Jens Peder Meldgaard <jpm@halfspace.io>
2025-06-02 13:21:28 +0200

    refactor: split logic into manageable pieces
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_common/src/geh_common/application/converters.py
+++ source/geh_common/src/geh_common/application/converters.py
@@ -14,8 +14,42 @@ def str_to_list(value: Any) -> list[str] | None:
     if not value:
         return None
     if isinstance(value, list):
-        return [str(item) for item in value]
+        return _convert_values(value)
     elif isinstance(value, str):
-        return re.findall(r"\d+", value)
+        return _convert_values(re.findall(r"\d+", value))
     else:
         raise TypeError(f"Input should be a valid list or string, not {type(value)}")
+
+
+def _convert_values(items) -> list[str] | None:
+    valid_values = []
+    for item in items:
+        if isinstance(item, int | str):
+            item_normalized = _normalize_scalar_value(item)
+            if item_normalized:
+                valid_values.append(item_normalized)
+        elif isinstance(item, list):
+            item_normalized = _convert_values(item)
+            if item_normalized:
+                valid_values.extend(item_normalized)
+    if not valid_values:
+        return None
+    return valid_values
+
+
+def _normalize_scalar_value(value: Any) -> str | None:
+    """Normalize a scalar value to a string or None.
+
+    Args:
+        value (Any): The input value to normalize.
+
+    Returns:
+        Optional[str]: The normalized string or None if the input is empty.
+    """
+    item = str(value)
+    if item.startswith("[") and item.endswith("]"):
+        item = item[1:-1]
+    item_normalized = str(item).strip()
+    if item_normalized and item_normalized != "[]":
+        return item_normalized
+    return None
--- source/geh_common/tests/application/types/test_energy_supplier_ids.py
+++ source/geh_common/tests/application/types/test_energy_supplier_ids.py
@@ -12,6 +12,18 @@ class ModelOptionalEnergySupplierIds(BaseModel):
     energy_supplier_ids: EnergySupplierIds | None = None
 
 
+def _assert_energy_supplier_ids(model, expected_ids):
+    """Helper function to assert the energy supplier IDs."""
+    if expected_ids is None:
+        assert model.energy_supplier_ids is None
+        return
+    if isinstance(expected_ids, str):
+        if expected_ids.startswith("[") and expected_ids.endswith("]"):
+            expected_ids = expected_ids[1:-1]
+        expected_ids = expected_ids.split(",")
+    assert model.energy_supplier_ids == expected_ids
+
+
 @pytest.mark.parametrize(
     "testcase, match",
     [
@@ -21,6 +33,9 @@ class ModelOptionalEnergySupplierIds(BaseModel):
         ("8000000000000,1234567890123456,1234567890123", None),
         ("123456789,1234567890123", "must be 13 or 16 characters"),
         ("", "Input should be a valid list"),
+        ("[8000000000000,1234567890123456,1234567890123]", None),
+        ("[123456789,1234567890123]", "must be 13 or 16 characters"),
+        ("[]", "Input should be a valid list"),
     ],
 )
 def test__required_energy_supplier_ids(testcase, match):
@@ -30,7 +45,7 @@ def test__required_energy_supplier_ids(testcase, match):
             ModelWithEnergySupplierIds(energy_supplier_ids=testcase)
     else:
         model = ModelWithEnergySupplierIds(energy_supplier_ids=testcase)
-        assert model.energy_supplier_ids == testcase.split(",") if isinstance(testcase, str) else testcase
+        _assert_energy_supplier_ids(model, testcase)
 
 
 @pytest.mark.parametrize(
@@ -43,6 +58,9 @@ def test__required_energy_supplier_ids(testcase, match):
         ("8000000000000,1234567890123456,1234567890123", None),
         ("123456789,1234567890123", "must be 13 or 16 characters"),
         ("", "Input should be a valid list"),
+        ("[8000000000000,1234567890123456,1234567890123]", None),
+        ("[123456789,1234567890123]", "must be 13 or 16 characters"),
+        ("[]", "Input should be a valid list"),
     ],
 )
 def test__optional_energy_supplier_ids(testcase, match):
@@ -52,7 +70,4 @@ def test__optional_energy_supplier_ids(testcase, match):
             ModelOptionalEnergySupplierIds(energy_supplier_ids=testcase)
     else:
         model = ModelOptionalEnergySupplierIds(energy_supplier_ids=testcase)
-        if testcase is None:
-            assert model.energy_supplier_ids is None
-        else:
-            assert model.energy_supplier_ids == testcase.split(",") if isinstance(testcase, str) else testcase
+        _assert_energy_supplier_ids(model, testcase)
