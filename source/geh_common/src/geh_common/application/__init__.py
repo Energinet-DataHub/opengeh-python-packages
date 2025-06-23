27476ba4e2c734eb400d5e850ce51074a3d2a92c
Jonas Michelsen <jvm@energinet.dk>
2025-06-23 14:31:14 +0200

    refac:  Add metering point types

--- source/geh_common/src/geh_common/application/__init__.py
+++ source/geh_common/src/geh_common/application/__init__.py
@@ -1,5 +1,6 @@
 from geh_common.application import settings
 from geh_common.application.types.energy_supplier_ids import EnergySupplierIds
 from geh_common.application.types.grid_area_codes import GridAreaCodes
+from geh_common.application.types.metering_point_types import MeteringPointTypes
 
-__all__ = ["settings", "GridAreaCodes", "EnergySupplierIds"]
+__all__ = ["settings", "GridAreaCodes", "EnergySupplierIds", "MeteringPointTypes"]
---
+++ source/geh_common/src/geh_common/application/types/metering_point_types.py
@@ -0,0 +1,42 @@
+from typing import Annotated
+
+from pydantic import AfterValidator, BeforeValidator
+from pydantic_settings import NoDecode
+
+from geh_common.application.converters.str_to_list import str_to_list
+from geh_common.domain.types import MeteringPointType
+
+
+def _validate_metering_point_types(v: list[str]) -> list[str]:
+    """Validate the list of energy supplier IDs."""
+    if v is None:
+        return v
+    for point in v:
+        if not isinstance(point, str):
+            raise TypeError(f"Metering point types must be strings, not {type(point)}")
+        if point not in [e.value for e in MeteringPointType]:
+            raise ValueError(
+                f"Unexpected metering point type: '{point}'. Must be one of: {', '.join([e.value for e in MeteringPointType])}"
+            )
+    return v
+
+
+MeteringPointTypes = Annotated[
+    list[str],
+    BeforeValidator(str_to_list),
+    AfterValidator(_validate_metering_point_types),
+    NoDecode(),
+]
+"""
+Annotated type for a list of metering point types.
+
+This type ensures that the input is converted to a list of strings representing metering point types,
+and that each type is validated to be a string that is represented in MeteringPointType.
+
+Validators:
+- BeforeValidator: Converts the input value to a list of metering point types.
+- AfterValidator: Validates that each metering point type is valid.
+- NoDecode: Prevents decoding of the input value.
+
+Example:
+"""
