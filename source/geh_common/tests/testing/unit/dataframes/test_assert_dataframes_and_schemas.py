718b64761574014a9c09f2e0bb84b8f9d7097adc
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2025-07-10 14:45:33 +0200

    feat: Add enforce row order to assert_dataframes_and_schemas (#199)

--- source/geh_common/tests/testing/unit/dataframes/test_assert_dataframes_and_schemas.py
+++ source/geh_common/tests/testing/unit/dataframes/test_assert_dataframes_and_schemas.py
@@ -94,3 +94,60 @@ def test__when_with_duplicated_rows_and_ignore_duplicated_rows_true__then_does_n
     # Act & Assert
     # Should not raise an exception when ignore_duplicated_rows is True
     assert_dataframes_and_schemas(actual, expected, configuration)
+
+
+def test__when_column_order_is_different__then_raises_exception(spark):
+    # Arrange
+    actual_schema = T.StructType(
+        [
+            T.StructField("aaa", T.StringType(), False),
+            T.StructField("bbb", T.StringType(), True),
+        ]
+    )
+    expected_schema = T.StructType(
+        [
+            T.StructField("bbb", T.StringType(), True),
+            T.StructField("aaa", T.StringType(), False),
+        ]
+    )
+
+    actual_data = [("a", "b"), ("c", "d"), ("e", "f")]
+    expected_data = [("a", "b"), ("c", "d"), ("e", "f")]
+    expected = spark.createDataFrame(expected_data, schema=expected_schema)
+    actual = spark.createDataFrame(actual_data, schema=actual_schema)
+
+    # Act & Assert
+    with pytest.raises(AssertionError) as excinfo:
+        assert_dataframes_and_schemas(actual, expected)
+
+    assert "Schema mismatch. Expected column name 'bbb', but found 'aaa'" in str(excinfo.value)
+
+
+def test__when_dataframe_row_order_mismatch__then_raises_exception(spark):
+    # Arrange
+    actual_schema = T.StructType(
+        [
+            T.StructField("aaa", T.StringType(), False),
+            T.StructField("bbb", T.StringType(), True),
+        ]
+    )
+    expected_schema = T.StructType(
+        [
+            T.StructField("aaa", T.StringType(), False),
+            T.StructField("bbb", T.StringType(), True),
+        ]
+    )
+
+    actual_data = [("a", "b"), ("c", "d"), ("e", "f")]
+    expected_data = [("c", "d"), ("a", "b"), ("e", "f")]
+    expected = spark.createDataFrame(expected_data, schema=expected_schema)
+    actual = spark.createDataFrame(actual_data, schema=actual_schema)
+
+    configuration = AssertDataframesConfiguration()
+    configuration.ignore_row_order = False
+
+    # Act & Assert
+    with pytest.raises(AssertionError) as excinfo:
+        assert_dataframes_and_schemas(actual, expected, configuration)
+
+    assert "Row mismatch at" in str(excinfo.value)
