6d7ca594ea2abab10373fddf0aad3df7f627d149
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-08-14 13:04:52 +0200

    feat: Add measurements_current_v2 (#207)

---
+++ source/geh_common/src/geh_common/data_products/measurements_core/measurements_gold/measurements_current_v2.py
@@ -0,0 +1,18 @@
+import pyspark.sql.types as t
+
+nullable = True
+
+database_name = "measurements_gold"
+
+view_name = "measurements_current_v2"
+
+schema = t.StructType(
+    # Fields are nullable, but we are checking for nulls in the view, except for quantity
+    [
+        t.StructField("metering_point_id", t.StringType(), not nullable),
+        t.StructField("observation_time", t.TimestampType(), not nullable),
+        t.StructField("quantity", t.DecimalType(18, 3), nullable),
+        t.StructField("quality", t.StringType(), not nullable),
+        t.StructField("metering_point_type", t.StringType(), not nullable),
+    ]
+)
