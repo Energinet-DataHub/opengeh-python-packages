35dae334af46ace44165c3ac521aaa36c1cc50f0
ClausQAEnerginet <xclpe@energinet.dk>
2025-10-07 14:12:09 +0200

    Update CovernatorTest.ps1

--- source/geh_common/src/geh_common/testing/covernator/CovernatorTest.ps1
+++ source/geh_common/src/geh_common/testing/covernator/CovernatorTest.ps1
@@ -12,4 +12,5 @@ python -c "import logging, runpy, sys; logging.basicConfig(level=logging.DEBUG, 
   C:\repo\opengeh-python-packages\source\geh_common\src\geh_common\covernator_streamlit\server.py `
   -g `
   -o "$desktop\covernator_out" `
-  -p C:\repo\opengeh-measurements\source\
+  -p C:\repo\opengeh-wholesale\source\
+#  -p C:\repo\opengeh-measurements\source\  
