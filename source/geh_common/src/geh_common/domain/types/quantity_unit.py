abc04423824b32e6d65fc648f9d22d3569154e27
Jonas Michelsen <jvm@energinet.dk>
2026-05-05 12:48:40 +0200

    .

--- source/geh_common/src/geh_common/domain/types/quantity_unit.py
+++ source/geh_common/src/geh_common/domain/types/quantity_unit.py
@@ -2,8 +2,13 @@ from enum import Enum
 
 
 class QuantityUnit(Enum):
+    AMPERE = "ampere"
+    STK = "stk"
     KWH = "kWh"
     KW = "kW"
     KVARH = "kVArh"
+    MW = "MW"
     MWH = "MWh"
+    MVAR = "MVAr"
     TONNE = "Tonne"
+    DANISH_TARIFF_CODE = "danish_tariff_code"
